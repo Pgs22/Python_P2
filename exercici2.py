9f2e808c306b120c3bf55b235dc8635d1cd1b06b
patricia <patricia.gsbcn@gmail.com>
2025-10-13 19:14:08 +0200

    ejercicios2

--- exercici2.py
+++ exercici2.py
@@ -3,15 +3,15 @@
 # 2.1 crea una llista de llistes per emmagatzemar entre 2 i 4 tasques pendents del dia d’avui assignant a cada tasca un temps en minuts. Per exemple:
 # tasques_pendents = [[35,"estudiar python"] ,[20,"plegar roba"] ]
 
-##
-2.2 mostra el temps assignat a la segona tasca
-2.3 mostra el número total de tasques
-2.4 mostra el nom de la última tasca
-2.5 assigna a la segona tasca un temps de 66 minuts i mostra la llista
-2.6 afegeix una nova tasca de 25 minuts que sigui "estudiar css" i mostra la llista
-2.7 suma els temps de la 1a i 3a tasca i mostra el resultat
-2.8 mostra el nom de la 2a i última tasca i mostra el resultat de sumar els seus temps
-2.9 inverteix l'ordre de les tasques i mostra la llista
-2.10 treu la 2a tasca amb pop()
-2.11 treu la 2a tasca amb remove()
+#
+#2.2 mostra el temps assignat a la segona tasca
+#2.3 mostra el número total de tasques
+#2.4 mostra el nom de la última tasca
+#2.5 assigna a la segona tasca un temps de 66 minuts i mostra la llista
+#2.6 afegeix una nova tasca de 25 minuts que sigui "estudiar css" i mostra la llista
+#2.7 suma els temps de la 1a i 3a tasca i mostra el resultat
+#2.8 mostra el nom de la 2a i última tasca i mostra el resultat de sumar els seus temps
+#2.9 inverteix l'ordre de les tasques i mostra la llista
+#2.10 treu la 2a tasca amb pop()
+#2.11 treu la 2a tasca amb remove()
 ##
