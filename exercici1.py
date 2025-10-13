0ad5147127cb0e2f7fb2be20056ab3a138875391
patricia <patricia.gsbcn@gmail.com>
2025-10-13 19:13:12 +0200

    practica

---
+++ exercici1.py
@@ -0,0 +1,15 @@
+#Afegeix en un arxiu de Python cadascuna de les següents sentències com un comentari. Després de cada comentari escriu el codi corresponent
+#1.1 crea una llista de nom "notes" que contingui una de les següents llistes de valors:
+#● 5,4,7,2,3,9
+#● 5,4,7,2,3
+#1.2 substitueix el 5 per un 1 i mostra la llista
+#1.3 mostra el valor de la última posició (utilitza len() per calcular la última posició)
+#1.4 utilitza index() per buscar i mostrar en quina posició està el número 4
+#1.5 utilitza append per afegir un nou valor al final de la llista i mostra-la
+#1.6 utilitza insert() per afegir el valor 88 després del 4 i mostra la llista
+#1.7 utilitza count per saber i mostrar quantes vegades apareix un valor
+#1.8 utilitza pop() per treure i mostrar el valor en la última posició
+#1.9 utilitza remove() per treure el valor 2 de l'array i mostra la llista
+#1.10 utilitza reverse() per invertir la posició dels valors dins l'array i mostra la llista
+#1.11 utilitza sum() i len() per calcular i mostrar el valor mitg dels valors dins l'array
+#1.12 mostra el valor situat al mig de l'array , si l'array te un número parell, arrodoneix la posició del mig cap amunt
